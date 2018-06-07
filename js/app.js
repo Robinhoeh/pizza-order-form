c15ff8f8d76aedb179441864d8b0e0c5b15d88de
Robin Watson <robinwatson@Robins-iMac.local>
2018-06-06 21:02:45 -0400

    pseudo code

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/* 
+lightbox
+form
+array
+timer runs down from when you hit order based on object holding 
+location and times
+
+1. what size of pizza? 
+s, m, l, xl
+	 - based on size there will be a fixed price
+2. What Dough do you want?
+ - wheat, regular, gluten free
+3. What sauce do you want?
+4. What cheese do you want?
+5. What Toppings do you want?
+5.1 randomize order button - replicate prophesize me random from each object
+6. Where do you want it delivered? 
+	input your address here
+7. Display order summary before submitting: 
+location
+price
+
+8. Display time it will take and  to order on submit
+9. Thank you for your order
+
+*/
