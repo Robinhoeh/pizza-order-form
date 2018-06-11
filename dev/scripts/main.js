4d40046473d1be7ada6786fc80b4e8ad10e8e461
RobinWatson <drumsrobin@gmail.com>
2018-06-11 09:39:59 -0400

    added reorder button

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,28 +1,5 @@
-/* 
-lightbox
-form
-array
-timer runs down from when you hit order based on object holding 
-location and times
-
-1. what size of pizza? 
-s, m, l, xl
-	 - based on size there will be a fixed price
-2. What Dough do you want?
- - wheat, regular, gluten free
-3. What sauce do you want?
-4. What cheese do you want?
-5. What Toppings do you want?
-5.1 randomize order button - replicate prophesize me random from each object
-6. Where do you want it delivered? 
-	input your address here
-7. Display order summary before submitting: 
-location
-price
-
-8. Display time it will take and  to order on submit
-9. Thank you for your order
-*/
+let countdown;
+const displayTimeRemaining = $('.display-time-remaining');
 
 $(function(){
 
@@ -51,6 +28,9 @@ $(function(){
 		
 		let inputButton = $(':input');
 		inputButton.val('Reorder');
+
+
+		
 	});	
 	
 	nextItemBtn.on('click', function(e){
@@ -59,5 +39,7 @@ $(function(){
 		const formItem = currentBtn.closest('.form-item');
 		formItem.addClass('next-item-animate');
 	});
+
+	
 	
 });
