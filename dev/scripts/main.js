780f16a88a16e3126f341346ba51b275a4ebcc67
Robin Watson <robinwatson@Robins-iMac.local>
2018-06-11 18:06:31 -0400

    adding countdown timer functionality

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,10 +1,22 @@
-let countdown;
-const displayTimeRemaining = $('.display-time-remaining');
 
-$(function(){
+const nextItemBtn = $('.next-item-button');
 
-	const nextItemBtn = $('.next-item-button');
-	
+function countdown(endDate) {
+	let days, hours, minutes, seconds;
+
+	endDate = new Date(endDate).getTime();
+
+	if(isNaN(endDate)) { return; }
+	 setInterval(calculate, 1000);
+
+	function calculate() {
+		let startDate = new Date();
+		startDate = startDate.getTime();
+		console.log(startDate);
+		let timeRemaining = Number((endDate - startDate) /1000);
+	}
+}
+
 	$('.form').on('submit', function(e){
 		e.preventDefault();
 		
@@ -24,13 +36,11 @@ $(function(){
 		<h3>Topping items: ${toppingItems}</h3>
 		<h3>Cheese type: ${cheeseType}</h3> 
 		<h3>Store location: ${storeType}</h3>`;
-		$('.order-summary').html(orderSummary);
+		$('.order-summary').html(orderSummaryMsg).append(orderSummary);
 		
 		let inputButton = $(':input');
 		inputButton.val('Reorder');
 
-
-		
 	});	
 	
 	nextItemBtn.on('click', function(e){
@@ -40,6 +50,4 @@ $(function(){
 		formItem.addClass('next-item-animate');
 	});
 
-	
-	
-});
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,35 +1,50 @@
 'use strict';
 
-var countdown = void 0;
-var displayTimeRemaining = $('.display-time-remaining');
+var nextItemBtn = $('.next-item-button');
 
-$(function () {
+function countdown(endDate) {
+	var days = void 0,
+	    hours = void 0,
+	    minutes = void 0,
+	    seconds = void 0;
 
-	var nextItemBtn = $('.next-item-button');
+	endDate = new Date(endDate).getTime();
 
-	$('.form').on('submit', function (e) {
-		e.preventDefault();
+	if (isNaN(endDate)) {
+		return;
+	}
+	setInterval(calculate, 1000);
 
-		// Define UI Variables
-		var pizzaSize = $('#pizzaSize option:selected').val();
-		var doughType = $('#doughType option:selected').val();
-		var sauceType = $('#sauceType option:selected').val();
-		var toppingItems = $('checkbox:checked').val();
-		var cheeseType = $('#cheeseType option:selected').val();
-		var storeType = $('#storeType option:selected').val();
+	function calculate() {
+		var startDate = new Date();
+		startDate = startDate.getTime();
+		console.log(startDate);
+		var timeRemaining = Number((endDate - startDate) / 1000);
+	}
+}
 
-		var orderSummaryMsg = '<h2>Thanks for ordering!</h2>';
-		var orderSummary = '<h3>Pizza size: ' + pizzaSize + '</h3>\n\t\t<h3>Dough Type: ' + doughType + '</h3> \n\t\t<h3>Sauce Type: ' + sauceType + '</h3>\n\t\t<h3>Topping items: ' + toppingItems + '</h3>\n\t\t<h3>Cheese type: ' + cheeseType + '</h3> \n\t\t<h3>Store location: ' + storeType + '</h3>';
-		$('.order-summary').html(orderSummary);
+$('.form').on('submit', function (e) {
+	e.preventDefault();
 
-		var inputButton = $(':input');
-		inputButton.val('Reorder');
-	});
+	// Define UI Variables
+	var pizzaSize = $('#pizzaSize option:selected').val();
+	var doughType = $('#doughType option:selected').val();
+	var sauceType = $('#sauceType option:selected').val();
+	var toppingItems = $('checkbox:checked').val();
+	var cheeseType = $('#cheeseType option:selected').val();
+	var storeType = $('#storeType option:selected').val();
 
-	nextItemBtn.on('click', function (e) {
-		e.preventDefault();
-		var currentBtn = $(e.currentTarget);
-		var formItem = currentBtn.closest('.form-item');
-		formItem.addClass('next-item-animate');
-	});
+	var orderSummaryMsg = '<h2>Thanks for ordering!</h2>';
+	var orderSummary = '<h3>Pizza size: ' + pizzaSize + '</h3>\n\t\t<h3>Dough Type: ' + doughType + '</h3> \n\t\t<h3>Sauce Type: ' + sauceType + '</h3>\n\t\t<h3>Topping items: ' + toppingItems + '</h3>\n\t\t<h3>Cheese type: ' + cheeseType + '</h3> \n\t\t<h3>Store location: ' + storeType + '</h3>';
+	$('.order-summary').html(orderSummaryMsg).append(orderSummary);
+
+	var inputButton = $(':input');
+	inputButton.val('Reorder');
+});
+
+nextItemBtn.on('click', function (e) {
+	e.preventDefault();
+	var currentBtn = $(e.currentTarget);
+	var formItem = currentBtn.closest('.form-item');
+	formItem.addClass('next-item-animate');
 });
