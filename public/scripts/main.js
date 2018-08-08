8880cefebc0ad0d022af36c73c11975877f1ae25
Robin Watson <robinwatson@Robins-iMac.local>
2018-08-07 22:44:41 -0400

    rebuilding countdown

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,41 +7,89 @@ pizzaApp.init = function () {
 	pizzaApp.nextItemBtn = $('.next-item-button');
 	
 	// countdown - hard coded
-	pizzaApp.countdown = function(endDate) {
-		let hours, minutes, seconds;
-	
-		pizzaApp.endDate = new Date(pizzaApp.endDate).getTime();
-	
-		if(isNaN(pizzaApp.endDate)) { 
-			return; 
-		}
-		
-		setInterval(pizzaApp.calculate, 1000);
-	
-		pizzaApp.calculate = function () {
-			let startDate = new Date();
-			startDate = startDate.getTime();
-	
-			let timeRemaining = parseInt((pizzaApp.endDate - startDate) /1000);
-	
-			if (timeRemaining >= 0) {
-	
-				pizzaApp.hours = parseInt(timeRemaining / 3600);
-				pizzaApp.timeRemaining = (timeRemaining % 3600);
-	
-				pizzaApp.minutes = parseInt(timeRemaining / 60);
-				pizzaApp.timeRemaining = (timeRemaining % 60);
-	
-				pizzaApp.seconds = parseInt(timeRemaining);
-	
-				document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-				document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-				document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-			} else{
-				return;
+	// Create a countdown timer for 35:00 minutes
+
+	pizzaApp.startTimer = function(duration, display) {
+		let timer = duration, minutes, seconds;
+		setInterval(function() {
+			minutes = parseInt(timer / 60, 10);
+			seconds = parseInt(timer % 60, 10);
+
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+
+			display.text(`${minutes}: ${seconds}`);
+
+			if (--timer < 0) {
+				timer = duration;
 			}
-		}
+		}, 1000);
 	}
+
+	$(function($) {
+		let thirtyFiveMinutes = 60 * 35,
+		display = $('.timer');
+		pizzaApp.startTimer( thirtyFiveMinutes, display);
+	});
+
+
+// 	function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.text(minutes + ":" + seconds);
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// jQuery(function ($) {
+//     var fiveMinutes = 60 * 5,
+//         display = $('#time');
+//     startTimer(fiveMinutes, display);
+// });
+	
+	// pizzaApp.countdown = function(endDate) {
+	// 	let hours, minutes, seconds;
+	
+	// 	pizzaApp.endDate = new Date(pizzaApp.endDate).getTime();
+	
+	// 	if(isNaN(pizzaApp.endDate)) { 
+	// 		return; 
+	// 	}
+		
+	// 	setInterval(pizzaApp.calculate, 1000);
+	
+	// 	pizzaApp.calculate = function () {
+	// 		let startDate = new Date();
+	// 		startDate = startDate.getTime();
+	
+	// 		let timeRemaining = parseInt((pizzaApp.endDate - startDate) /1000);
+	
+	// 		if (timeRemaining >= 0) {
+	
+	// 			pizzaApp.hours = parseInt(timeRemaining / 3600);
+	// 			pizzaApp.timeRemaining = (timeRemaining % 3600);
+	
+	// 			pizzaApp.minutes = parseInt(timeRemaining / 60);
+	// 			pizzaApp.timeRemaining = (timeRemaining % 60);
+	
+	// 			pizzaApp.seconds = parseInt(timeRemaining);
+	
+	// 			document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+	// 			document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+	// 		} else{
+	// 			return;
+	// 		}
+	// 	}
+	// }
 	
 	// on load - focus on input
 	$('#pizzaSize').focus();
